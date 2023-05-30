53dd2e862bbd7360c90271a5dc1a6dbf3fb66f38
muhammadderic <muhammad.donny.e@gmail.com>
2023-05-30 09:59:04 +0700

    Change navbar menu to humberger menu for mobile

---
+++ script.js
@@ -0,0 +1,19 @@
+const rightBar = document.querySelector(".right-bar");
+const hamburger = document.querySelector(".hamburger");
+const menuItems = document.querySelectorAll(".link");
+const closeIcon = document.querySelector(".closeIcon");
+const menuIcon = document.querySelector(".menuIcon");
+
+function toggleMenu() {
+  if (rightBar.classList.contains("showMenu")) {
+    rightBar.classList.remove("showMenu");
+    closeIcon.style.display = "none";
+    menuIcon.style.display = "block";
+  } else {
+    rightBar.classList.add("showMenu");
+    closeIcon.style.display = "block";
+    menuIcon.style.display = "none";
+  }
+}
+
+hamburger.addEventListener("click", toggleMenu);
